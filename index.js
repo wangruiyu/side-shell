5f052838a1690d8e5ef0b965f33fd05806e2b468
wangry <wangruiyu1990@163.com>
2017-06-24 13:52:50 +0800

    统一side类名

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
  */
 $(function(){
     $(".show-btn").click(function(){
-        $(".slide-shell-wrap").addClass("animate-show");
+        $(".side-shell-wrap").addClass("animate-show");
         $(".close-btn-wrap").addClass("animate-show");
         $(".mask").show();
         $("html").css({"width":"100%","height":"100%","overflow":"hidden"});
     });
     $(".close-btn").click(function () {
-        $(".slide-shell-wrap").removeClass("animate-show");
+        $(".side-shell-wrap").removeClass("animate-show");
         $(".close-btn-wrap").removeClass("animate-show");
         $(".mask").hide();
         $("html").css({"width":"100%","height":"auto","overflow":"auto"});
